91c80813cbd71f72f72f030f6d92019a637df16f
Victor Ponamarev <vponamarev@guess.com>
2022-03-19 20:42:03 +0300

    OrderLineItemModel: Name property added.

--- src/ChannelEngineTopSellingProducts.Infrastructure/ApiIntegration/Models/OrderLineItemModel.cs
+++ src/ChannelEngineTopSellingProducts.Infrastructure/ApiIntegration/Models/OrderLineItemModel.cs
@@ -2,6 +2,7 @@
 
 public sealed class OrderLineItemModel
 {
+	public string Name { get; init; } = string.Empty;
 	public string Gtin { get; init; } = string.Empty;
 	public string MerchantProductNo { get; init; } = string.Empty;
 	public string ChannelProductNo { get; init; } = string.Empty;
