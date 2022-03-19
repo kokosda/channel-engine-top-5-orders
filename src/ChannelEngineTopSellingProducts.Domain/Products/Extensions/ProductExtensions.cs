8efee554e419e5db560a8631beb1078ff8217dd4
Victor Ponamarev <vponamarev@guess.com>
2022-03-20 01:01:44 +0300

    Domain: Product and ProductExtensions added because the algorithm uses them.

---
+++ src/ChannelEngineTopSellingProducts.Domain/Products/Extensions/ProductExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ChannelEngineTopSellingProducts.Domain.Products.Extensions;
+
+public static class ProductExtensions
+{
+	public static TopSellingProduct ToTopSellingProduct(this Product product, int totalQuantity)
+	{
+		var result = new TopSellingProduct
+		{
+			Id = product.Id,
+			Name = product.Name,
+			MerchantProductNo = product.MerchantProductNo,
+			Gtin = product.Gtin,
+			TotalQuantity = totalQuantity
+		};
+		return result;
+	}
+}
---
+++ src/ChannelEngineTopSellingProducts.Domain/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ChannelEngineTopSellingProducts.Domain.Products;
+
+public sealed class Product
+{
+	public string Id { get; init; } = string.Empty;
+	public string Name { get; init; } = string.Empty;
+	public string MerchantProductNo { get; init; } = string.Empty;
+	public string Gtin { get; init; } = string.Empty;
+	public int Quantity { get; init; }
+}
