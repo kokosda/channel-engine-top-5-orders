013206cb9145a46584ff9039fcf9b4628779066b
Victor Ponamarev <vponamarev@guess.com>
2022-03-20 01:08:51 +0300

    TopSellingProductViewModel: ID property added to better differentiate products in the HTML table.

---
+++ src/ChannelEngineTopSellingProducts.Web/Models/Extensions/TopSellingProductViewModelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ChannelEngineTopSellingProducts.Application.Products;
+
+namespace ChannelEngineTopSellingProducts.Web.Models.Extensions;
+
+public static class TopSellingProductViewModelExtensions
+{
+	public static TopSellingProductViewModel ToViewModel(this TopSellingProductDto topSellingProductDto)
+	{
+		if (topSellingProductDto == null)
+			throw new ArgumentNullException(nameof(topSellingProductDto));
+
+		var result = new TopSellingProductViewModel
+		{
+			Id = topSellingProductDto.Id,
+			Name = topSellingProductDto.Name,
+			Gtin = topSellingProductDto.Gtin,
+			TotalQuantity = topSellingProductDto.TotalQuantity
+		};
+
+		return result;
+	}
+}
--- src/ChannelEngineTopSellingProducts.Web/Models/TopSellingProductViewModel.cs
+++ src/ChannelEngineTopSellingProducts.Web/Models/TopSellingProductViewModel.cs
@@ -2,6 +2,7 @@
 
 public sealed class TopSellingProductViewModel
 {
+	public string Id { get; set; } = string.Empty;
 	public string Name { get; init; } = string.Empty;
 	public string Gtin { get; init; } = string.Empty;
 	public int TotalQuantity { get; init; }
