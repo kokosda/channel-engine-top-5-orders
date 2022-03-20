e7fe3e17c342b4890c7a75c2678c83a23c3369ee
Victor Ponamarev <vponamarev@guess.com>
2022-03-20 09:51:36 +0300

    TopSellingProductPriorityQueueBasedAlgorithm: converting topSellingProductCandidates to the list instead of the dictionary as it is not needed.

--- src/ChannelEngineTopSellingProducts.Domain/Products/TopSellingProductPriorityQueueBasedAlgorithm.cs
+++ src/ChannelEngineTopSellingProducts.Domain/Products/TopSellingProductPriorityQueueBasedAlgorithm.cs
@@ -9,28 +9,28 @@ public sealed class TopSellingProductPriorityQueueBasedAlgorithm : ITopSellingPr
 	{
 		var priorityQueue = new PriorityQueue<TopSellingProduct, int>();
 		var topSellingProductCandidates = products.GroupBy(p => p.Id, p => p)
-			.ToImmutableDictionary(g => g.Key, GroupToTopSellingProduct);
+			.Select(GroupToTopSellingProduct)
+			.ToImmutableList();
 
-		foreach (var (_, topSellingProduct) in topSellingProductCandidates.Take(topCount))
+		foreach (var topSellingProduct in topSellingProductCandidates.Take(topCount))
 		{
 			priorityQueue.Enqueue(topSellingProduct, topSellingProduct.TotalQuantity);
 		}
 
-		foreach (var (_, topSellingProduct) in topSellingProductCandidates.Skip(topCount))
+		foreach (var topSellingProduct in topSellingProductCandidates.Skip(topCount))
 		{
 			priorityQueue.EnqueueDequeue(topSellingProduct, topSellingProduct.TotalQuantity);
 		}
 
-		var stack = new Stack<TopSellingProduct>();
+		var result = new Stack<TopSellingProduct>();
 		var priorityQueueEntriesCount = priorityQueue.Count;
 
 		for (var i = 0; i < priorityQueueEntriesCount; i++)
 		{
 			var element = priorityQueue.Dequeue();
-			stack.Push(element);
+			result.Push(element);
 		}
 
-		var result = stack.ToImmutableList();
 		return result;
 	}
 
